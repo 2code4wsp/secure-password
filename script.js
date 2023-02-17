c46b22839df5125cee9c3c2eb0557c9664055b8d
2code4wsp <tlynde1043@gmail.com>
2023-02-17 09:55:10 -0800

    js code written

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const numericChar = ['0','1','2','3','4','5','6','7','8','9']
 const upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 const specialChar = ['!','@','#','$','%','^','&','*','(',')','_','-','=','+','?','/','.','{','}','[',']','<','>']
 const lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+let characterNum
+let charCodes
 //do i need to change const to let because they will be pulled by getRandom? 
-  let charCodes = "";
+
   
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -18,30 +20,47 @@ function writePassword() {
 
 }
 function generatePassword() {
-
-  let (characterNum) = prompt("How many characters in the password");
-  console.log((characterNum) > 8 && (characterNum) < 128) {
-    alert("Password must be between 8-128 characters in length");
-    }
-  
-  let upperCase = prompt("do you want to use uppercase characters in your password? Press cancel for no.");
- console.log()
-
-  let lowerCase = prompt("do you want to use lowercase characters in your password? Press cancel for no.");
+  charCodes = [];
+  characterNum = prompt("How many characters in the password");
+  console.log(characterNum)
  
-  let numericChar = prompt("Numeric characters?");
+  //alert("Password must be between 8-128 characters in length");
+   // }
+  let useUpperCase = confirm("do you want to use uppercase characters in your password? Press cancel for no.");
+ console.log("use upper case", useUpperCase)
+ if (useUpperCase) {
+  charCodes = charCodes.concat(upperCase);
+  console.log(charCodes)
+ }
 
-  let specialChar = prompt("do you want to use special characters in your password? Press cancel for no.");
-  
+  let useLowerCase = confirm("do you want to use lowercase characters in your password? Press cancel for no.");
+  console.log("use lower case", useLowerCase)
+ if (useLowerCase) {
+  charCodes = charCodes.concat(lowerCase)
+  console.log(charCodes)
+ }
+ 
+  let useNumericChar = confirm("Numeric characters?");
+  if (useNumericChar) {
+    charCodes = charCodes.concat(numericChar)
+    console.log(charCodes)
+  }
 
+  let useSpecialChar = confirm("Special characters in your password?");
+  if (useSpecialChar) {
+    charCodes = charCodes.concat(specialChar)
+    console.log(charCodes)
+  }
+return getRandom()
 }
 
 function getRandom() {
-  for (let i = 0; i < characterNum.length; i++) {
-     charCodes[Math.floor(Math.random() * charCodes.length)]
-     return Math.random() * (128 - 8);
+  let password=""
+  for (let i = 0; i < characterNum; i++) {
+     let randomchar = charCodes[Math.floor(Math.random() * charCodes.length)]
+    password = password.concat(randomchar)
   }
-   return "password"
+   return password
 }
 
 // Add event listener to generate button
